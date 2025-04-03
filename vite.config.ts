60113a2690d81a608894936cfe1ef352c2d7aa65
Benjamín Eidelman <beneidel+gh@gmail.com>
2025-04-03 20:22:26 -0300

    using @hono/vite-dev-server

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { cloudflare } from '@cloudflare/vite-plugin'
 import { defineConfig } from 'vite'
-import ssrHotReload from 'vite-plugin-ssr-hot-reload'
+
+import build from '@hono/vite-cloudflare-pages'
+import devServer from '@hono/vite-dev-server'
+import adapter from '@hono/vite-dev-server/cloudflare'
+
+// import { cloudflare } from '@cloudflare/vite-plugin'
+// import ssrHotReload from 'vite-plugin-ssr-hot-reload'
 
 export default defineConfig({
-  plugins: [ssrHotReload(), cloudflare()]
+  plugins: [
+    // ssrHotReload(), cloudflare()
+    devServer({
+      entry: 'src/index.tsx',
+      adapter, // Cloudflare Adapter
+    }),
+    build(),
+  ]
 })
